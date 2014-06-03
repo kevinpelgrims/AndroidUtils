009ff2b7208194edf525454b773b8f113d49d4e5
Kevin Pelgrims <kp@drivr.com>
2014-06-03 07:49:34 +0200

    Add date formatting

--- library/src/main/java/com/kevinpelgrims/utils/library/FormatHelper.java
+++ library/src/main/java/com/kevinpelgrims/utils/library/FormatHelper.java
@@ -1,7 +1,12 @@
 package com.kevinpelgrims.utils.library;
 
+import android.text.format.DateUtils;
+
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FormatHelper {
     public static String formatDecimal(String format, double value) {
@@ -15,5 +20,16 @@ public class FormatHelper {
 
     public static String formatCurrencyNoDecimals(double value, String currency) {
         return String.format("%s %s", currency, formatDecimalNoDecimals(value));
+    }
+
+    public static String formatDate(Date date, String format) {
+        if (date == null) return "";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
+    }
+
+    public static CharSequence formatDateToRelativeTimeSpan(Date date) {
+        if (date == null) return "";
+        return DateUtils.getRelativeTimeSpanString(date.getTime(), Calendar.getInstance().getTimeInMillis(), DateUtils.MINUTE_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE);
     }
 }
